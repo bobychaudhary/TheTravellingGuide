da2a458d3c8c7d3bdb5c8d9dbb9d17303935a588
bobychaudhary <bobychaudhary708@gmail.com>
2021-11-27 21:40:15 +0530

    new project environment has been added

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer bg-info w-100 h-100" >
+
+            </footer>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Footer from './Components/Footer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Footer/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
