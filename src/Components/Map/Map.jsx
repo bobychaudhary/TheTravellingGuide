c1b2030a14ffaf5f1a0a7f61ed4c25eba89ea5e8
bobychaudhary <bobychaudhary708@gmail.com>
2021-11-29 10:48:14 +0530

    map has been added

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react';
 
-const Map = () => {
-    return (
-        <div>
-            <h1>Map</h1>
-        </div>
-    )
-}
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+class Map extends Component{
+    static defaultProps={
+        center:{
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
+    render(){
+        return (
+          <div style={{ height: "100vh", width: "100%" }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: process.env.REACT_APP_THETRAVELLINGGUIDE,
+              }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+              <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text="Humari Website"
+              />
+            </GoogleMapReact>
+          </div>
+        );}
+    }
+
 
 export default Map
